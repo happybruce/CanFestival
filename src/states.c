648cf9e156fa9cfd765f05deee2bccd501cb6a97
WangHao <hao-wang@siemens-healthineers.com>
2024-08-27 17:54:37 +0800

    Use space to replace tab

--- src/states.c
+++ src/states.c
@@ -42,8 +42,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ** @param newCommunicationState                                                                    
 **/     
 void switchCommunicationState(CO_Data* d, 
-	s_state_communication *newCommunicationState);
-	
+    s_state_communication *newCommunicationState);
+    
 /*!                                                                                                
 **                                                                                                 
 **                                                                                                 
@@ -53,7 +53,7 @@ void switchCommunicationState(CO_Data* d,
 **/    
 e_nodeState getState(CO_Data* d)
 {
-  return d->nodeState;
+    return d->nodeState;
 }
 
 /*!                                                                                                
@@ -64,71 +64,71 @@ e_nodeState getState(CO_Data* d)
 **/  
 void canDispatch(CO_Data* d, Message *m)
 {
-	UNS16 cob_id = UNS16_LE(m->cob_id);
-	 switch(cob_id >> 7)
-	{
-		case SYNC:		/* can be a SYNC or a EMCY message */
-			if(cob_id == 0x080)	/* SYNC */
-			{
-				if(d->CurrentCommunicationState.csSYNC)
-					proceedSYNC(d);
-			} else 		/* EMCY */
-				if(d->CurrentCommunicationState.csEmergency)
-					proceedEMCY(d,m);
-			break;
-		case TIME_STAMP:
-		case PDO1tx:
-		case PDO1rx:
-		case PDO2tx:
-		case PDO2rx:
-		case PDO3tx:
-		case PDO3rx:
-		case PDO4tx:
-		case PDO4rx:
-			if (d->CurrentCommunicationState.csPDO)
-				proceedPDO(d,m);
-			break;
-		case SDOtx:
-		case SDOrx:
-			if (d->CurrentCommunicationState.csSDO)
-				proceedSDO(d,m);
-			break;
-		case NODE_GUARD:
-			if (d->CurrentCommunicationState.csLifeGuard)
-				proceedNODE_GUARD(d,m);
-			break;
-		case NMT:
-			if (*(d->iam_a_slave))
-			{
-				proceedNMTstateChange(d,m);
-			}
+    UNS16 cob_id = UNS16_LE(m->cob_id);
+    switch(cob_id >> 7)
+    {
+        case SYNC:        /* can be a SYNC or a EMCY message */
+            if(cob_id == 0x080)    /* SYNC */
+            {
+                if(d->CurrentCommunicationState.csSYNC)
+                    proceedSYNC(d);
+            } else         /* EMCY */
+                if(d->CurrentCommunicationState.csEmergency)
+                    proceedEMCY(d,m);
+            break;
+        case TIME_STAMP:
+        case PDO1tx:
+        case PDO1rx:
+        case PDO2tx:
+        case PDO2rx:
+        case PDO3tx:
+        case PDO3rx:
+        case PDO4tx:
+        case PDO4rx:
+            if (d->CurrentCommunicationState.csPDO)
+                proceedPDO(d,m);
+            break;
+        case SDOtx:
+        case SDOrx:
+            if (d->CurrentCommunicationState.csSDO)
+                proceedSDO(d,m);
+            break;
+        case NODE_GUARD:
+            if (d->CurrentCommunicationState.csLifeGuard)
+                proceedNODE_GUARD(d,m);
+            break;
+        case NMT:
+            if (*(d->iam_a_slave))
+            {
+                proceedNMTstateChange(d,m);
+            }
             break;
 #ifdef CO_ENABLE_LSS
-		case LSS:
-			if (!d->CurrentCommunicationState.csLSS)break;
-			if ((*(d->iam_a_slave)) && cob_id==MLSS_ADRESS)
-			{
-				proceedLSS_Slave(d,m);
-			}
-			else if(!(*(d->iam_a_slave)) && cob_id==SLSS_ADRESS)
-			{
-				proceedLSS_Master(d,m);
-			}
-			break;
+        case LSS:
+            if (!d->CurrentCommunicationState.csLSS)break;
+            if ((*(d->iam_a_slave)) && cob_id==MLSS_ADRESS)
+            {
+                proceedLSS_Slave(d,m);
+            }
+            else if(!(*(d->iam_a_slave)) && cob_id==SLSS_ADRESS)
+            {
+                proceedLSS_Master(d,m);
+            }
+            break;
 #endif
-	}
+    }
 }
 
 #define StartOrStop(CommType, FuncStart, FuncStop) \
-	if(newCommunicationState->CommType && d->CurrentCommunicationState.CommType == 0){\
-		MSG_WAR(0x9999,#FuncStart, 9999);\
-		d->CurrentCommunicationState.CommType = 1;\
-		FuncStart;\
-	}else if(!newCommunicationState->CommType && d->CurrentCommunicationState.CommType == 1){\
-		MSG_WAR(0x9999,#FuncStop, 9999);\
-		d->CurrentCommunicationState.CommType = 0;\
-		FuncStop;\
-	}
+    if(newCommunicationState->CommType && d->CurrentCommunicationState.CommType == 0){\
+        MSG_WAR(0x9999,#FuncStart, 9999);\
+        d->CurrentCommunicationState.CommType = 1;\
+        FuncStart;\
+    }else if(!newCommunicationState->CommType && d->CurrentCommunicationState.CommType == 1){\
+        MSG_WAR(0x9999,#FuncStop, 9999);\
+        d->CurrentCommunicationState.CommType = 0;\
+        FuncStop;\
+    }
 #define None
 
 /*!                                                                                                
@@ -136,18 +136,18 @@ void canDispatch(CO_Data* d, Message *m)
 **                                                                                                 
 ** @param d                                                                                        
 ** @param newCommunicationState                                                                    
-**/  	
+**/      
 void switchCommunicationState(CO_Data* d, s_state_communication *newCommunicationState)
 {
 #ifdef CO_ENABLE_LSS
-	StartOrStop(csLSS,	startLSS(d),	stopLSS(d))
+    StartOrStop(csLSS,    startLSS(d),    stopLSS(d))
 #endif
-	StartOrStop(csSDO,	None,		resetSDO(d))
-	StartOrStop(csSYNC,	startSYNC(d),		stopSYNC(d))
-	StartOrStop(csLifeGuard,	lifeGuardInit(d),	lifeGuardStop(d))
-	StartOrStop(csEmergency,	emergencyInit(d),	emergencyStop(d)) 
-	StartOrStop(csPDO,	PDOInit(d),	PDOStop(d))
-	StartOrStop(csBoot_Up,	None,	slaveSendBootUp(d))
+    StartOrStop(csSDO,    None,        resetSDO(d))
+    StartOrStop(csSYNC,    startSYNC(d),        stopSYNC(d))
+    StartOrStop(csLifeGuard,    lifeGuardInit(d),    lifeGuardStop(d))
+    StartOrStop(csEmergency,    emergencyInit(d),    emergencyStop(d)) 
+    StartOrStop(csPDO,    PDOInit(d),    PDOStop(d))
+    StartOrStop(csBoot_Up,    None,    slaveSendBootUp(d))
 }
 
 /*!                                                                                                
@@ -160,62 +160,62 @@ void switchCommunicationState(CO_Data* d, s_state_communication *newCommunicatio
 **/  
 UNS8 setState(CO_Data* d, e_nodeState newState)
 {
-	if(newState != d->nodeState){
-		switch( newState ){
-			case Initialisation:
-			{
-				s_state_communication newCommunicationState = {1, 0, 0, 0, 0, 0, 0};
-				d->nodeState = Initialisation;
-				switchCommunicationState(d, &newCommunicationState);
-				/* call user app init callback now. */
-				/* d->initialisation MUST NOT CALL SetState */
-				(*d->initialisation)(d);				
-			}
+    if(newState != d->nodeState){
+        switch( newState ){
+            case Initialisation:
+            {
+                s_state_communication newCommunicationState = {1, 0, 0, 0, 0, 0, 0};
+                d->nodeState = Initialisation;
+                switchCommunicationState(d, &newCommunicationState);
+                /* call user app init callback now. */
+                /* d->initialisation MUST NOT CALL SetState */
+                (*d->initialisation)(d);                
+            }
 
-			/* Automatic transition - No break statement ! */
-			/* Transition from Initialisation to Pre_operational */
-			/* is automatic as defined in DS301. */
-			/* App don't have to call SetState(d, Pre_operational) */
-								
-			case Pre_operational:
-			{
-				
-				s_state_communication newCommunicationState = {0, 1, 1, 1, 1, 0, 1};
-				d->nodeState = Pre_operational;
-				switchCommunicationState(d, &newCommunicationState);
+            /* Automatic transition - No break statement ! */
+            /* Transition from Initialisation to Pre_operational */
+            /* is automatic as defined in DS301. */
+            /* App don't have to call SetState(d, Pre_operational) */
+                                
+            case Pre_operational:
+            {
+                
+                s_state_communication newCommunicationState = {0, 1, 1, 1, 1, 0, 1};
+                d->nodeState = Pre_operational;
+                switchCommunicationState(d, &newCommunicationState);
                 (*d->preOperational)(d);
-			}
-			break;
-								
-			case Operational:
-			if(d->nodeState == Initialisation) return 0xFF;
-			{
-				s_state_communication newCommunicationState = {0, 1, 1, 1, 1, 1, 0};
-				d->nodeState = Operational;
-				newState = Operational;
-				switchCommunicationState(d, &newCommunicationState);
-				(*d->operational)(d);
-			}
-			break;
-						
-			case Stopped:
-			if(d->nodeState == Initialisation) return 0xFF;
-			{
-				s_state_communication newCommunicationState = {0, 0, 0, 0, 1, 0, 1};
-				d->nodeState = Stopped;
-				newState = Stopped;
-				switchCommunicationState(d, &newCommunicationState);
-				(*d->stopped)(d);
-			}
-			break;
-			default:
-				return 0xFF;
+            }
+            break;
+                                
+            case Operational:
+            if(d->nodeState == Initialisation) return 0xFF;
+            {
+                s_state_communication newCommunicationState = {0, 1, 1, 1, 1, 1, 0};
+                d->nodeState = Operational;
+                newState = Operational;
+                switchCommunicationState(d, &newCommunicationState);
+                (*d->operational)(d);
+            }
+            break;
+                        
+            case Stopped:
+            if(d->nodeState == Initialisation) return 0xFF;
+            {
+                s_state_communication newCommunicationState = {0, 0, 0, 0, 1, 0, 1};
+                d->nodeState = Stopped;
+                newState = Stopped;
+                switchCommunicationState(d, &newCommunicationState);
+                (*d->stopped)(d);
+            }
+            break;
+            default:
+                return 0xFF;
 
-		}/* end switch case */
-	
-	}
-	/* d->nodeState contains the final state */
-	/* may not be the requested state */
+        }/* end switch case */
+    
+    }
+    /* d->nodeState contains the final state */
+    /* may not be the requested state */
     return d->nodeState;  
 }
 
@@ -228,7 +228,7 @@ UNS8 setState(CO_Data* d, e_nodeState newState)
 **/ 
 UNS8 getNodeId(CO_Data* d)
 {
-  return *d->bDeviceNodeId;
+    return *d->bDeviceNodeId;
 }
 
 /*!                                                                                                
@@ -244,17 +244,17 @@ void setNodeId(CO_Data* d, UNS8 nodeId)
 #ifdef CO_ENABLE_LSS
   d->lss_transfer.nodeID=nodeId;
   if(nodeId==0xFF){
-  	*d->bDeviceNodeId = nodeId;
-  	return;
+      *d->bDeviceNodeId = nodeId;
+      return;
   }
   else
 #endif
   if(!(nodeId>0 && nodeId<=127)){
-	  MSG_WAR(0x2D01, "Invalid NodeID",nodeId);
-	  return;
+      MSG_WAR(0x2D01, "Invalid NodeID",nodeId);
+      return;
   }
 
-  if(offset){
+  if(offset) {
     /* Adjust COB-ID Client->Server (rx) only id already set to default value or id not valid (id==0xFF)*/
     if((READ_UNS32(d->objdict, offset, 1) == ((UNS32)0x600) + *d->bDeviceNodeId)||(*d->bDeviceNodeId==0xFF)){
       /* cob_id_client = 0x600 + nodeId; */
@@ -268,12 +268,12 @@ void setNodeId(CO_Data* d, UNS8 nodeId)
   }
 
   /* 
-   	Initialize the server(s) SDO parameters
-  	Remember that only one SDO server is allowed, defined at index 0x1200	
- 		
-  	Initialize the client(s) SDO parameters 	
-  	Nothing to initialize (no default values required by the DS 401)	
-  	Initialize the receive PDO communication parameters. Only for 0x1400 to 0x1403 
+      Initialize the server(s) SDO parameters
+      Remember that only one SDO server is allowed, defined at index 0x1200    
+         
+      Initialize the client(s) SDO parameters     
+      Nothing to initialize (no default values required by the DS 401)    
+      Initialize the receive PDO communication parameters. Only for 0x1400 to 0x1403 
   */
   {
     UNS8 i = 0;
@@ -286,9 +286,9 @@ void setNodeId(CO_Data* d, UNS8 nodeId)
       canID = READ_UNS32(d->objdict, offset, 1) & 0x1fffffff;
       otherBits = READ_UNS32(d->objdict, offset, 1) & ~0x1fffffff;
       if((canID == cobID[i] + *d->bDeviceNodeId)||(*d->bDeviceNodeId==0xFF))
-	      WRITE_UNS32(d->objdict, offset, 1, (cobID[i] + nodeId) | otherBits);
-      i ++;
-      offset ++;
+          WRITE_UNS32(d->objdict, offset, 1, (cobID[i] + nodeId) | otherBits);
+      i++;
+      offset++;
     }
   }
   /* ** Initialize the transmit PDO communication parameters. Only for 0x1800 to 0x1803 */
@@ -304,9 +304,9 @@ void setNodeId(CO_Data* d, UNS8 nodeId)
       canID = READ_UNS32(d->objdict, offset, 1) & 0x1fffffff;
       otherBits = READ_UNS32(d->objdict, offset, 1) & ~0x1fffffff;
       if((canID == cobID[i] + *d->bDeviceNodeId)||(*d->bDeviceNodeId==0xFF))
-	      WRITE_UNS32(d->objdict, offset, 1, (cobID[i] + nodeId) | otherBits);
-      i ++;
-      offset ++;
+          WRITE_UNS32(d->objdict, offset, 1, (cobID[i] + nodeId) | otherBits);
+      i++;
+      offset++;
     }
   }
 
