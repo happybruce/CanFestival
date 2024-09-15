0ddc93924d522c24c453f66e2476950a4f6c6f96
happybruce <whahu1989@126.com>
2024-09-15 16:52:12 +0800

    Add declaration.h to solve repeated definition problem

---
+++ include/declaration.h
@@ -0,0 +1,9 @@
+#ifndef __DECLARATION_H__
+#define __DECLARATION_H__
+
+// Add below declaration to solve repeated definition problem
+typedef struct struct_CO_Data CO_Data;
+typedef struct Message Message;
+
+
+#endif
