197ae2c0a1a9b144cac56610c2a7ee07fd3eee62
wanghao <hao-wang@siemens-healthineers.com>
2024-11-06 17:13:58 +0800

    Optimize timer.h/c

--- include/timer.h
+++ include/timer.h
@@ -35,10 +35,10 @@ extern "C" {
 #define TIMER_HANDLE INTEGER16
 
 /* --------- types and constants definitions --------- */
-#define TIMER_FREE 0
-#define TIMER_ARMED 1
-#define TIMER_TRIG 2
-#define TIMER_TRIG_PERIOD 3
+#define TIMER_FREE   0
+#define TIMER_ARMED  1
+#define TIMER_TRIG   2
+#define TIMER_TRIG_PERIOD  3
 
 #define TIMER_NONE -1
 
